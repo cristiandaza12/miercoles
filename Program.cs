61cb38e9f9adf06554df758bf6642353b6199b9a
unknown <Sala300@TUNDFPCGE10F360.FormacionSena.RED>
2019-08-28 17:29:02 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace miercoles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("bienvenidisimo");
+            {
+                Mueble mu = new Mueble();
+                mu.Color = "cafe";
+                mu.Tamaño = "grande";
+                mu.Tipo = "mesa";
+                Console.WriteLine($"color = {mu.Color}");
+                Console.WriteLine($"tamaño = {mu.Tamaño}");
+                Console.WriteLine($"tipo = {mu.Tipo}");
+                mu.modificar("negro", "silla");
+                mu.imprimirmueble();
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ mueble.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace miercoles
+{
+
+    class Mueble
+    {
+        public string Tipo { get; set; }
+
+        public string Color { get; set; }
+
+        public string Tamaño { get; set; }
+
+        public Mueble()
+        {
+
+        }
+
+        public Mueble(string tipo, string color, string tamaño)
+        {
+            Tipo = tipo;
+            Color = color;
+            Tamaño = tamaño;
+        }
+
+        public Mueble(string tipo, string tamaño)
+        {
+            Tipo = tipo;
+            Tamaño = tamaño;
+        }
+        #region metodos
+        public void ingresarmueble()
+        {
+            Console.WriteLine("ingrese color");
+            Color = Console.ReadLine();
+            Console.WriteLine("ingrese tamaño");
+            Tamaño = Console.ReadLine();
+            Console.WriteLine("ingrese tipo");
+            Tipo = Console.ReadLine();
+
+
+        }
+        
+        public void imprimirmueble()
+        {
+            Console.WriteLine($"color: {Color}");
+            Console.WriteLine($"tamaño = {Tamaño}");
+            Console.WriteLine($"tipo = {Tipo}");
+        }
+
+        #endregion
+
+        public void modificar(string color, string tipo)
+        {
+            Color = color;
+            Tipo = tipo;
+        }
+
+
+    }
+
+}
